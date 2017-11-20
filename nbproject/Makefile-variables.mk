2ccaaf72bd2684fd4df3bef2ec8c17dde680d63f
Dmitry Pavlov <dpavlov@iaaras.ru>
2017-11-20 18:18:22 +0300

    DDE NetBeans project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,51 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=libdde.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/libdde.so
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=libdde.so.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/libdde.so.tar
+# Release32 configuration
+CND_PLATFORM_Release32=GNU-Linux
+CND_ARTIFACT_DIR_Release32=dist/Release32/GNU-Linux
+CND_ARTIFACT_NAME_Release32=libdde.so
+CND_ARTIFACT_PATH_Release32=dist/Release32/GNU-Linux/libdde.so
+CND_PACKAGE_DIR_Release32=dist/Release32/GNU-Linux/package
+CND_PACKAGE_NAME_Release32=libdde.so.tar
+CND_PACKAGE_PATH_Release32=dist/Release32/GNU-Linux/package/libdde.so.tar
+# Release64 configuration
+CND_PLATFORM_Release64=GNU-Linux
+CND_ARTIFACT_DIR_Release64=dist/Release64/GNU-Linux
+CND_ARTIFACT_NAME_Release64=libdde.so
+CND_ARTIFACT_PATH_Release64=dist/Release64/GNU-Linux/libdde.so
+CND_PACKAGE_DIR_Release64=dist/Release64/GNU-Linux/package
+CND_PACKAGE_NAME_Release64=libdde.so.tar
+CND_PACKAGE_PATH_Release64=dist/Release64/GNU-Linux/package/libdde.so.tar
+# Release64LongDouble configuration
+CND_PLATFORM_Release64LongDouble=GNU-Linux
+CND_ARTIFACT_DIR_Release64LongDouble=dist/Release64LongDouble/GNU-Linux
+CND_ARTIFACT_NAME_Release64LongDouble=libdde.so
+CND_ARTIFACT_PATH_Release64LongDouble=dist/Release64LongDouble/GNU-Linux/libdde.so
+CND_PACKAGE_DIR_Release64LongDouble=dist/Release64LongDouble/GNU-Linux/package
+CND_PACKAGE_NAME_Release64LongDouble=libdde.so.tar
+CND_PACKAGE_PATH_Release64LongDouble=dist/Release64LongDouble/GNU-Linux/package/libdde.so.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
